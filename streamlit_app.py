ef0dfdce9926c4fc3b37ee580bb7f0d81ee9b95c
kekov1 <128424616+kekov1@users.noreply.github.com>
2023-03-21 19:46:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
