0ba1200389bd6d85a276c5e88c926285c54ffd93
kekov1 <128424616+kekov1@users.noreply.github.com>
2023-03-21 19:40:30 -0300

    Update streamlit_app.py
    Vuelta a la versión que funcionaba.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
